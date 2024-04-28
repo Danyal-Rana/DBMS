02599a1d062f9c1b3286162a4f199ec160f3f731
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-04-28 17:45:11 +0500

    Apna College

--- MySQL/PQ_3.sql
+++ MySQL/PQ_3.sql
@@ -4,3 +4,15 @@ ALTER TABLE student2
 CHANGE name fullName VARCHAR(50);
 
 SELECT * FROM student2;
+
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM student2
+WHERE marks < 80;
+
+SELECT * FROM student2;
+
+ALTER TABLE student2
+DROP COLUMN grade;
+
+SELECT * FROM student2;
