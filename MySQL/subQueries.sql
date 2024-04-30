d09b65c06e25a2ea3660d79c4f7135290b4a546e
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-04-30 23:15:14 +0500

    Apna College

--- MySQL/subQueries.sql
+++ MySQL/subQueries.sql
@@ -17,3 +17,42 @@ WHERE marks >
 SELECT AVG(marks)
 FROM student2
 );
+
+-- 2nd sub query
+SELECT fullName, rollNo
+FROM student2
+WHERE rollNo%2 = 0;
+
+SELECT fullName, rollNo
+FROM student2
+WHERE rollNo IN 
+(
+	SELECT rollNo
+    FROM student2
+    WHERE rollNo%2=0
+);
+
+-- 3rd sub query
+SELECT *
+FROM student2
+WHERE city="delhi";
+
+SELECT MAX(marks)
+FROM (
+	SELECT *
+    FROM student2
+    WHERE city="delhi"
+) AS temp;
+
+-- VIEW 
+CREATE VIEW view1 AS
+SELECT fullName, city
+FROM student2;
+
+SELECT * 
+FROM view1
+WHERE city="delhi";
+
+DROP VIEW view1;
+
+SELECT * FROM view1;
